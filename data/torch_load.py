9fcf5ee497f81ea8695b52d063f6e14b14621386
moxime <mossonce@hotmail.com>
2020-03-03 13:11:22 +0100

    torch load

---
+++ data/torch_load.py
@@ -0,0 +1,39 @@
+from torchvision import datasets, transforms
+
+
+simple_transform = transforms.Compose([transforms.ToTensor(),
+                                       transforms.Lambda(lambda x: x
+                                                         / 255.0)])
+
+
+def get_dataset(dataset='MNIST', root='./data', ood=None):
+
+    if dataset == 'MNIST':
+
+        set_getter = datasets.MNIST
+        transform = simple_transform
+
+    if dataset == 'fashion':
+        set_getter = datasets.FashionMNIST
+        transform = simple_transform
+        
+    trainset = set_getter(root=root, train=True,
+                          download=True,
+                          transform=transform)
+
+    testset = set_getter(root=root, train=False,
+                         download=True,
+                         transform=transform)
+
+    return trainset, testset
+
+
+def get_mnist(**kw):
+
+    return get_dataset(dataset='MNIST', **kw)
+
+
+def get_fashion_mnist(**kw):
+
+    return get_dataset(dataset='fashion', **kw)
+

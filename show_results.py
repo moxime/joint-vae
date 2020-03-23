d78a1fb5ec2fcaa86580393bb327f17f46fb167d
moxime <mossonce@hotmail.com>
2020-03-23 18:32:17 +0100

    ...

--- show_results.py
+++ show_results.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from cvae import ClassificationVariationalNetwork
 from data import torch_load as torchdl
 from utils.save_load import collect_networks, get_path_from_input
-from roc_curves import ood_roc, miss_roc, fpr_at_tpr, tpr_at_fpr
+from roc_curves import ood_roc, miss_roc, fpr_at_tpr, tpr_at_fpr, plot_roc
 import argparse
 import torch
 from data.torch_load import choose_device

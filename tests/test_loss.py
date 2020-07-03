6aa9b1bcc49c72ca5b4b8cb0c4cc2a0de48f2bac
moxime <mossonce@hotmail.com>
2020-07-03 14:36:27 +0200

    test_loss

--- tests/test_loss.py
+++ tests/test_loss.py
@@ -1,41 +1,45 @@
-import tensorflow as tf
-import numpy as np
-from tensorflow.keras.layers import Layer, Dense, Concatenate, Input
-from tensorflow.keras.models import Model
-from tensorflow.keras.losses import mse, categorical_crossentropy as xent
-from data import generate as dg
+from cvae import ClassificationVariationalNetwork as Net
+import utils.losses as losses
+import torch
+import torch.nn.functional as F
+
+C = 2
+D = (11, 5)
+K = 7
+L = 10000
+
+N = 50
+
+
+x = torch.randn(N, *D)
+
+y = torch.randint(0, C, (N,))
+
+
+beta = 1e-2
+net = Net(D, C, latent_dim=K, latent_sampling=L, beta=beta)
+
+# y_ = torch.cat([torch.zeros(1, N), torch.ones(1, N)], axis=0)
+
+_x_, logit_, mu_z, lv_z, z_ = net(x, y)
+y_ = F.softmax(logit_, -1)
 
 
 
-class MixedModel(Model):
+mse_loss = ((_x_ - x) ** 2).mean()
+x_loss = 0
 
-    def __init__(self, **kw):
+for n in range(N):
+    y_n = y[n]
+    x_loss += - y_[:, n, y_n].log().mean(axis=0) / N
 
-        super().__init__(**kw)
-        self.dense_1 = Dense(64, activation='relu', name='dense_1')
-        self.dense_2 = Dense(64, activation='relu', name='latent')
-        self.predict =  Dense(10, activation='softmax', name='predictions')
-        self.reconstruct = Dense(784, name='reconstructed')
+kl_loss = 0.5 * (lv_z.exp() + mu_z ** 2 - 1 - lv_z).sum(axis=-1).mean()
 
-    def call(self, inputs):
+total_loss = mse_loss + 2 * beta * x_loss + 2 * beta * kl_loss
 
-        x_input = inputs[0]
-        y_input = inputs[1]
-        o1 = self.dense_1(x_input)
-        o2 = self.dense_2(o1)
-        y_ = self.predict(o2)
-        x_ = self.reconstruct(o2)
-        self.add_loss(mse(x_input, x_))
-        self.add_loss(xent(y_, y_input))
-        
-        return [x_, y_]
+losses = net.loss(x, y, _x_, y_, mu_z, lv_z, return_all_losses=True)
 
-model = MixedModel()
+_x, logit, loss = net.evaluate(x)
 
 
-(x_train, y_train, x_test, y_test) = dg.get_mnist() 
 
-# model.compile(loss=[None, categorical_crossentropy], optimizer='Adam')
-model.compile(optimizer='Adam')
-
-model.fit([x_train, y_train], epochs=10)

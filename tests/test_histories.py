1f60a3195ae5657240b5eefa339463c5097231f8
moxime <mossonce@hotmail.com>
2020-07-24 10:01:16 +0200

    test histories

---
+++ tests/test_histories.py
@@ -0,0 +1,13 @@
+nets = []
+
+collect_networks('jobs/fashion', nets)
+
+nets = sum(nets, [])
+
+for n in nets:
+
+    n_ = n['net']
+    train_loss = n_.train_history['train_loss']
+    test_accuracy = n_.train_history['test_accuracy']
+
+    print(len(train_loss), '--', len(test_accuracy))

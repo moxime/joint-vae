dfeb1154ff7e2eebc4152393dc793e714def105c
moxime <mossonce@hotmail.com>
2020-10-09 08:57:58 +0200

    test_one réparé

--- test_one.py
+++ test_one.py
@@ -37,33 +37,19 @@ y_ = {}
 losses = {}
 measures = {}
 
-logpx_std = {}
-logpx_mean = {}
-logpx_min = {}
-logpx_max = {}
-logpx_delta = {}
-logpx_nstd = {}
-
 methods = ('max', 'std', 'mag', 'mean', 'nstd', 'IYx')
-
-
 sets = ('test', 'ood')
 
 types = ('mean', 'max', 'std', 'delta', 'nstd')
-logpx_ = (logpx_mean, logpx_max, logpx_std, logpx_delta, logpx_nstd)
-
 
 print('*\n' * 10)
 
 
-for t, logpx in zip(types, logpx_):
-    print(f'\n*** {t} ***')
-    for s in sets:
-        print(s)
-        print(' | '.join(f'{l:-7.2f}' for l in logpx[s]))
+
+for s in sets:
+    print(s)
 
     x_[s], y_[s], losses[s], _ = net.evaluate(x[s])
-
     measures[s] = net.batch_dist_measures(None, losses[s], methods)
                   
 
@@ -73,5 +59,5 @@ for m in methods:
         print(s)
         print('|'.join(f'{l:-7.2f}' for l in measures[s][m]) )
         
-net.ood_detection_rates(batch_size=20, num_batch=20)
+# net.ood_detection_rates(batch_size=20, num_batch=20)
 

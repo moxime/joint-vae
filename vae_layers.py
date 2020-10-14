894c93cf985835b522bce06946bd205c5348dcc8
moxime <mossonce@hotmail.com>
2020-10-14 13:18:04 +0200

    added batch_norm in vgg

--- vae_layers.py
+++ vae_layers.py
@@ -97,7 +97,7 @@ class VGGFeatures(nn.Sequential):
                 w = w // 2
             else:
                 layers += [nn.Conv2d(in_channels, x, kernel_size=3, padding=1),
-                           # nn.BatchNorm2d(x),
+                           nn.BatchNorm2d(x),
                            nn.ReLU(inplace=True)]
                 in_channels = x
         layers += [nn.AvgPool2d(kernel_size=1, stride=1)]

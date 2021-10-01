144b59987129ad276267438cd7fc43431840aae8
moxime <mossonce@hotmail.com>
2021-10-01 12:40:02 +0200

    faster fpr by sorting measures

--- utils/roc_curves.py
+++ utils/roc_curves.py
@@ -50,18 +50,17 @@ def roc_curve(ins, outs, *kept_tpr, two_sided=False, around='mean', validation=1
     val_ins_idx = permute_ins[:ins_n_valid]
     test_ins_idx = permute_ins[ins_n_valid:]
     
-    sorted_ins = np.sort(ins[test_ins_idx])
     ins_validation = np.asarray(ins)[val_ins_idx]
 
     if two_sided:
         if around == 'mean':
             center = ins_validation.mean()
+        all_thresholds = np.concatenate([[-np.inf], np.sort(abs(ins[test_ins_idx] - center))])
+        sorted_outs = np.sort(abs(outs - center))
             
-    if two_sided:
-        all_thresholds = np.concatenate([[0], np.sort(abs(sorted_ins - center)), [np.inf]])
-        
     else:
-        all_thresholds = np.concatenate([[-np.inf], sorted_ins, [np.inf]])[::-1]
+        all_thresholds = np.concatenate([[-np.inf], np.sort(-ins[test_ins_idx])])
+        sorted_outs = np.sort(-outs)
 
     relevant_thresholds = []
     relevant_fpr = []
@@ -74,35 +73,45 @@ def roc_curve(ins, outs, *kept_tpr, two_sided=False, around='mean', validation=1
     kept_thresholds = np.zeros_like(kept_tpr)
     kept_tpr_i = 0
 
-    n_ins = len(sorted_ins)
+    n_ins = len(all_thresholds) - 1
     n_outs = len(outs)
-
+        
     if debug == 'time':
         t0 = time()
         print('*** Going through thresholds')
+
+    outs_idx = 0
         
-    for i, t in enumerate(all_thresholds):
+    for ins_idx, t in enumerate(all_thresholds):
 
-        tpr = i / n_ins
-        if two_sided:
-            # tpr = (abs(sorted_ins - center) <= t).sum() / n_ins
-            fpr = (abs(outs - center) <= t).sum() / n_outs
-        else:
-            # tpr = (sorted_ins >= t).sum() / n_ins
-            fpr = (outs >= t).sum() / n_outs
+        while outs_idx < n_outs and sorted_outs[outs_idx] < t:
+            outs_idx += 1
 
+        tpr = (ins_idx) / n_ins 
+        fpr = (outs_idx) / n_outs
+
+        if not two_sided: t = -t
         if debug == 'hard':
-            print('{:5.2f} -- {:5.2f}'.format(100 * fpr, 100 * tpr), end=' ')
+            if two_sided:
+                tpr_ = (abs(ins[test_ins_idx] - center) <= t).sum() / n_ins
+                fpr_ = (abs(outs - center) <= t).sum() / n_outs
+            else:
+                tpr_ = (ins[test_ins_idx] >= t).sum() / n_ins
+                fpr_ = (outs >= t).sum() / n_outs
+
+            print('   {:7.4f} -- {:7.4f}'.format(100 * fpr, 100 * tpr))
+            print('___{:7.4f} -- {:7.4f}'.format(100 * fpr_, 100 * tpr_), end=' ')
+            
         if fpr >= last_fpr:
             relevant_thresholds.append(t)
             relevant_tpr.append(tpr)
             relevant_fpr.append(fpr)
             last_fpr = fpr
             if debug == 'hard':
-                print('*')
+                print('*___')
 
         elif debug == 'hard':
-            print()
+            print('___')
 
         if kept_tpr_i < len(kept_tpr) and tpr >= kept_tpr[kept_tpr_i]:
 
@@ -113,14 +122,14 @@ def roc_curve(ins, outs, *kept_tpr, two_sided=False, around='mean', validation=1
 
             if debug in ('medium', 'hard'):
 
-                for what, data in zip(('TPR', 'FPR'), (sorted_ins, outs)):
+                for what, data in zip(('TPR', 'FPR'), (ins, outs)):
 
                     n = len(data)
                     if two_sided:
 
                         t1 = center - t
                         t2 = center + t
-                        bt = (data < center - t).sum() / n * 100
+                        bt = (data <= center - t).sum() / n * 100
                         at = (data > center + t).sum() / n * 100
                         pr = 100 - at - bt
                         _s = f'{what}: {bt:5.2f} ({t1:+.2e}) {pr:5.2f} ({t2:+.2e}) {at:5.2f}'
@@ -141,7 +150,6 @@ def roc_curve(ins, outs, *kept_tpr, two_sided=False, around='mean', validation=1
     if debug == 'time':
         print(f'AUC computed in {time() - t0:3f}s')
 
-    
     if two_sided:
         kept_thresholds = np.concatenate([np.array([center]), kept_thresholds])
         
@@ -154,14 +162,14 @@ if __name__ == '__main__':
     import sklearn.metrics
     logging.getLogger().setLevel(logging.DEBUG)
     
-    n_ = [15000, 10000]
+    n_ = [1, 100-1]
     s_ = [1, 0.5]
     m_ = [2, 2]
     
     ins = np.concatenate([np.random.randn(n) * s + m for (n, s, m) in zip(n_, s_, m_)])
     ins = np.random.permutation(ins)
     
-    outs = np.random.randn(25000) + 1
+    outs = np.random.randn(250) + 1
 
     inandouts = np.concatenate([ins, outs])
     labels = np.concatenate([np.ones_like(ins), np.zeros_like(outs)])
@@ -169,21 +177,41 @@ if __name__ == '__main__':
     kept_tpr = [_ / 100 for _ in range(90, 100)]
 
     offset = 2
-    for two_sided, factor in zip((False,) + (True,) * 1, (1,) + (1,) + (1,) * 1):
+
+    two_sided_ = (True, False)
+    factor_ = (1, 1)
+
+    # two_sided_ = (False,)
+    # two_sided_ = (True,)
+    # factor_ = (1,)
+    
+    for two_sided, factor in zip(two_sided_, factor_):
+        
         print('2S' if two_sided else '1S')
         t0 = time()
         auroc, fpr, tpr, thr = roc_curve(factor * ins + offset, outs, *kept_tpr,
-                                         debug='time',
-                                         two_sided=two_sided, validation=1000)
+                                         debug=False,
+                                         two_sided=two_sided, validation=1)
 
         t_home_made = time() - t0
         
         print('AUC={:.2f}'.format(100 * auroc))
+
+        if two_sided:
+            center = thr[0]
+            thr = thr[1:]
+            
         for h, f, t in zip(thr, fpr, tpr):
 
-            f_ = (outs >= h).sum() / len(outs)
-            t_ = (ins >= h).sum() / len(ins)
-            print(('{:7.3f}--{:7.3f} ').format(100 * f, 100 * t))
+            print((' {:7.3f}--{:7.3f} ').format(100 * f, 100 * t))
+            if not two_sided: 
+                f_ = (outs >= h).sum() / len(outs)
+                t_ = (factor * ins + offset >= h).sum() / len(ins)
+            else:
+                f_ = (abs(outs - center) <= h).sum() / len(outs)
+                t_ = (abs(factor * ins + offset - center) <= h).sum() / len(ins)
+                
+            print(('_{:7.3f}--{:7.3f} ').format(100 * f_, 100 * t_))
 
         t0 = time()
         sklearn.metrics.roc_curve(labels, inandouts)

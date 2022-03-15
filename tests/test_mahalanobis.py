8b976f866441f2eb6608bb7c4ad18ee47e57a805
moxime <mossonce@hotmail.com>
2022-03-15 14:57:39 +0100

    include file

---
+++ tests/test_mahalanobis.py
@@ -0,0 +1,53 @@
+import sys
+import argparse
+import logging
+from utils.parameters import parse_filters
+from utils.filters import DictOfListsOfParamFilters
+from utils.save_load import load_json, needed_remote_files
+
+rmodels = load_json('jobs', 'models-home.json')
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--last', default=0, type=int)
+    parser.add_argument('-v', action='count', default=1)
+
+    args_from_file = ('--dataset cifar10 '
+                      '--type cvae '
+                      '--gamma 500 '
+                      '--sigma-train coded '
+                      '--coder-dict learned '
+                      '--last 1'
+                      # '--job-num 149127'
+                      ).split()
+
+    args, ra = parser.parse_known_args(None if len(sys.argv) > 1 else args_from_file)
+
+    logging.getLogger().setLevel(40 - 10 * args.v)
+    
+    filter_parser = parse_filters()
+    filter_args = filter_parser.parse_args(ra)
+
+    filters = DictOfListsOfParamFilters()
+
+    for _ in filter_args.__dict__:
+        filters.add(_, filter_args.__dict__[_])
+
+    mdirs = [_ for _ in rmodels if filters.filter(rmodels[_])][-args.last:]
+
+    total_models = len(mdirs)
+    with open('/tmp/files', 'w') as f:
+
+        for mdir, sdir in needed_remote_files(*mdirs, epoch='last', which_rec='ind', state=True):
+            if mdir in mdirs:
+                mdirs.remove(mdir)
+            f.write(sdir + '\n')
+
+    print(len(mdirs), 'complete model' + ('s' if len(mdirs) > 1 else ''), 'over', total_models)
+    
+    if not mdirs:
+        logging.warning('Exiting, load files')
+        logging.warning('E.g: %s', '$ rsync -avP --files-from=/tmp/files remote:dir/joint-vae .')
+

582fcf165a0a9ba1fdc43caac98341e1eb7d082f
moxime <mossonce@hotmail.com>
2022-06-14 14:58:06 +0200

    debug

--- cvae.py
+++ cvae.py
@@ -280,8 +280,8 @@ class ClassificationVariationalNetwork(nn.Module):
             learned_latent_prior_means = False
             latent_prior_means = 0
 
-        assert latent_prior_variance in ('scalar', 'diag', 'full'), print('LPM', latent_prior_variance)
-
+        assert latent_prior_variance in ('scalar', 'diag', 'full'), print('LPV', latent_prior_variance)
+        
         self.encoder = Encoder(encoder_input_shape, num_labels,
                                intermediate_dims=encoder_layer_sizes,
                                latent_dim=latent_dim,

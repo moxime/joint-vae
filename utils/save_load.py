7bf2d95603f2f46da69e878ef2b2c2d631eb807d
moxime <mossonce@hotmail.com>
2022-07-05 13:43:00 +0200

    load on cpu

--- utils/save_load.py
+++ utils/save_load.py
@@ -758,7 +758,7 @@ def available_results(model,
     for epoch in results:
         rec_dir = os.path.join(sample_dir, sample_sub_dirs.get(epoch, 'false_dir'))
         if os.path.isdir(rec_dir):
-            recorders = LossRecorder.loadall(rec_dir)
+            recorders = LossRecorder.loadall(rec_dir, map_location='cpu')
             # epoch = last_samples(model)
             for s, r in recorders.items():
                 # print('***', s)

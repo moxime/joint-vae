23c7a2ad18e8f62f6137b6a6d12009c789097bd0
moxime <mossonce@hotmail.com>
2022-07-05 14:30:02 +0200

    debug

--- cvae.py
+++ cvae.py
@@ -1306,7 +1306,7 @@ class ClassificationVariationalNetwork(nn.Module):
 
             elif log:
 
-                logging.debug(f'Accuracies not updated')
+                logging.debug(f'Accuracies not updated bc {}<={}'.format(n, n_laready))
 
         return acc[m] if only_one_method else acc
 

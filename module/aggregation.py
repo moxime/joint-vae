a69e7eb584338ea7e1ea07d647c65946ab90c2cf
moxime <mossonce@hotmail.com>
2022-11-15 12:42:38 +0100

    ok

--- module/aggregation.py
+++ module/aggregation.py
@@ -54,33 +54,68 @@ def latent_mutual_info(m1, m2, x, y):
     assert m1.is_cvae
     assert m2.is_cvae
     assert m1.input_shape == m2.input_shape
-
+    assert m1.num_labels == m2.num_labels
     models = {0: m1, 1: m2}
 
+    sampling = {_: models[_].latent_sampling for _ in models}
+
+    C = m1.num_labels
     z = {}
     muz = {}
     y_ = {}
-    posterior = {}
+    pyz = {}
+
+    stacked_y = {_: torch.stack([c * torch.ones((sampling[_], len(x)),
+                                                dtype=int,
+                                                device=x.device)
+                                 for c in range(C)], dim=0)
+                 for _ in models}
 
     for _ in models:
-
         m = models[_]
+        other = (_ + 1) % 2
+        thisL = 'L' + str(_)
+        otherL = 'L' + str(other)
+
         outs = m.forward(x)
-        z[_] = outs[-1]
+
+        # z is of shape LxMxK
+        z[_] = outs[-1][1:]
+
+        z = z[_].expand(C, *z[_].shape)
         muz[_] = outs[-2]
 
-        my = m.encoder.prior.mean.rename('y', 'K')
+        # names: 'y', thisL, 'M'
+        logpzy = m.encoder.prior.log_density(z, stacked_y[_])
+
+        max_logpzy = logpzy.max(0)[0]
+
+        dlogpzy = logpzy - max_logpzy
+        pyz[_] = dlogpzy.exp() / dlogpzy.exp().sum(0)
+
+        y_[_] = logpzy.mean(1).argmax(0)
+
+        # print('{:.1%}'.format((y_[_] == y).sum() / len(y)))
+
+        pyz[_] = pyz[_].unsqueeze(1).expand(-1, sampling[other], -1, -1).rename('y', otherL, thisL, 'M')
+
+    pyz[1] = pyz[1].align_as(pyz[0])
+
+    Im = (pyz[0] * pyz[1]).sum('y').log().mean(('L0', 'L1'))
+
+    return Im, y_[0]
 
 
 if __name__ == '__main__':
 
+    import os
     import sys
     import time
     import logging
     import warnings
     import argparse
-    from utils.torch_load import get_dataset
-    from utils.save_load import find_by_job_number, needed_remote_files
+    from utils.torch_load import get_dataset, get_same_size_by_name
+    from utils.save_load import find_by_job_number, needed_remote_files, LossRecorder
     from cvae import ClassificationVariationalNetwork as M
 
     parser = argparse.ArgumentParser()
@@ -90,7 +125,7 @@ if __name__ == '__main__':
     parser.add_argument('--batch-size', '-M', default=256, type=int)
     parser.add_argument('-N', default=2000, type=int)
 
-    args_from_file = '-vv 226397 226180 -N 2000 -M 64'.split()
+    args_from_file = '-vv 226397 226180 -N 2000 -M 32 -N 1000'.split()
 
     args = parser.parse_args(None if len(sys.argv) > 1 else args_from_file)
 
@@ -134,27 +169,40 @@ if __name__ == '__main__':
     for _ in m_:
         _.to(args.device)
 
-    _, dataset = get_dataset(params['set'], transformer=params['transformer'], splits=['test'])  #
+    dir_name = os.path.join('parallel-jobs', '|'.join(str(_) for _ in sorted(jobs)))
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
 
-    loader = torch.utils.data.DataLoader(dataset, shuffle=True,
-                                         batch_size=args.batch_size)
+    sets = [params['set']]
 
-    n = 0
-    t0 = time.time()
-    for x, y in loader:
+    sets.extend(get_same_size_by_name(params['set']))
 
-        n += len(x)
+    batch_size = args.batch_size
 
-        if n >= args.N:
-            break
-        out = latent_mutual_info(*m_, x.to(device), y.to(device))
+    for s in sets:
 
-        t1 = time.time()
-        t_per_i = (t1 - t0) / n
-        eta = (args.N - n) * t_per_i
-        print('{:4}/{:4} -- {:.3f} ms/i -- eta {:.0f}s   '.format(n, args.N, 1000 * t_per_i, eta), end='\r')
+        recorder = LossRecorder(batch_size)
 
-    else:
-        logging.warning('Only {} images processed'.format(n))
+        _, dataset = get_dataset(s, transformer=params['transformer'], splits=['test'])  #
+        loader = torch.utils.data.DataLoader(dataset, shuffle=True,
+                                             batch_size=batch_size)
 
-    print('Processed {} images in {:.0f}s ({:.0f}ms/i)'.format(n, t1 - t0, 1000 * (t1 - t0) / n))
+        n = 0
+        t0 = time.time()
+        for x, y in loader:
+
+            n += len(x)
+
+            if n >= args.N:
+                break
+            Im, y_ = latent_mutual_info(*m_, x.to(device), y.to(device))
+
+            recorder.append_batch(Im=Im.rename(None), y_=y_.rename(None))
+
+            t1 = time.time()
+            t_per_i = (t1 - t0) / n
+            eta = (args.N - n) * t_per_i
+            print('{:4}/{:4} -- {:.3f} ms/i -- eta {:.0f}s   '.format(n, args.N, 1000 * t_per_i, eta), end='\r')
+
+        print('Processed {} images of {} in {:.0f}s ({:.0f}ms/i)'.format(n, s, t1 - t0, 1000 * (t1 - t0) / n))
+        recorder.save(os.path.join(dir_name, 'record-{}.pth'.format(s)))

e74418b4437b2a21a3be8d5d0fb182a0f1383c89
moxime <mossonce@hotmail.com>
2023-02-17 13:57:54 +0100

    to be tested

--- module/priors.py
+++ module/priors.py
@@ -5,46 +5,6 @@ from utils.print_log import texify_str
 import logging
 
 
-class TiltedGaussianPrior(GaussianPrior):
-
-    def __init__(self, dim, num_priors=1, mean=0, learned_means=False, tau=0):
-
-        super().__init__(dim,
-                         num_priors=num_priors,
-                         mean=mean,
-                         learned_means=learned_means,
-                         var_type='scalar',
-                         learned_variance=False)
-
-        self.tau = tau
-        self.mu_star = None
-
-    def log_density(self, z, y=None):
-
-        return super().log_density(z, y) - z.pow(2).sum(-1).sqrt
-
-    @property
-    def mu_star(self):
-        return self.tau
-
-    def kl(self, mu, log_var, y=None, output_dict=True, var_weighting=1.):
-
-        if var_weighting != 1.:
-            logging.debug('var weighting != 1 but tilted gaussian does not care')
-
-        if y is not None and y.ndim == mu.ndim:
-            expand_shape = list(mu.unsqueeze(0).shape)
-            expand_shape[0] = y.shape[0]
-            return self.kl(mu.expand(*expand_shape), log_var.expand(*expand_shape),
-                           y=y, output_dict=output_dict)
-
-        mu_norm = self.mahala(mu, y).sqrt()
-        kl = 0.5 * (mu_norm - self.mu_star) ** 2
-
-        loss_components = {'mu_norm': mu_norm, 'kl': kl}
-        return loss_components if output_dict else loss_components['kl']
-
-
 class GaussianPrior(nn.Module):
 
     def __init__(self, dim, var_type='scalar', num_priors=1,
@@ -304,3 +264,43 @@ class GaussianPrior(nn.Module):
         plural = 's' if self.conditional else ''
         mean = ('learned ' if self.learned_means else '') + 'mean' + plural
         return '{p}prior of dim {K} with {m} and {v}'.format(p=pre, m=mean, v=var, K=self.dim)
+
+
+class TiltedGaussianPrior(GaussianPrior):
+
+    def __init__(self, dim, num_priors=1, mean=0, learned_means=False, tau=0):
+
+        super().__init__(dim,
+                         num_priors=num_priors,
+                         mean=mean,
+                         learned_means=learned_means,
+                         var_type='scalar',
+                         learned_variance=False)
+
+        self.tau = tau
+        self.mu_star = None
+
+    def log_density(self, z, y=None):
+
+        return super().log_density(z, y) - z.pow(2).sum(-1).sqrt
+
+    @property
+    def mu_star(self):
+        return self.tau
+
+    def kl(self, mu, log_var, y=None, output_dict=True, var_weighting=1.):
+
+        if var_weighting != 1.:
+            logging.debug('var weighting != 1 but tilted gaussian does not care')
+
+        if y is not None and y.ndim == mu.ndim:
+            expand_shape = list(mu.unsqueeze(0).shape)
+            expand_shape[0] = y.shape[0]
+            return self.kl(mu.expand(*expand_shape), log_var.expand(*expand_shape),
+                           y=y, output_dict=output_dict)
+
+        mu_norm = self.mahala(mu, y).sqrt()
+        kl = 0.5 * (mu_norm - self.mu_star) ** 2
+
+        loss_components = {'mu_norm': mu_norm, 'kl': kl}
+        return loss_components if output_dict else loss_components['kl']

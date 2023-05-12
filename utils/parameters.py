0f4ae82144a023d0dd53962e9f11da34080ee81d
moxime <mossonce@hotmail.com>
2023-05-12 11:04:45 +0200

    remove index

--- utils/parameters.py
+++ utils/parameters.py
@@ -446,7 +446,7 @@ def get_args_for_results(argv=None):
 
     parser.add_argument('--predict-methods', nargs='*', default=None)
 
-    parser.add_argument('--remove-index', nargs='*')
+    parser.add_argument('--remove-index', nargs='*', default=['auto'])
 
     args, ra = parser.parse_known_args(argv)
 

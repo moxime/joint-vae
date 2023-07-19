caad26ea5b6686e331d7230c471b6f00b96394d1
moxime <mossonce@hotmail.com>
2023-07-19 15:09:19 +0200

    conv layers

--- tests/test_conv_layer.py
+++ tests/test_conv_layer.py
@@ -10,8 +10,8 @@ shape = (3, 32, 32)
 conv_name = '4x3-ax2'
 conv_name = '4x3:1+4-ax2'
 conv_name = '64x3-Mx2-128x3-Mx2-256x3-256x3-Mx2-512x3-512x3-Mx2-512x3-512x3-Mx2-Ax1'
-
 conv_name = '[x3-Mx2]64-M-128-M-256-256-M-512-512-M-512-512-M-Ax1'
+conv_name = 'conv32'
 
 conv = build_de_conv_layers(shape, conv_name)
 
@@ -30,7 +30,10 @@ deconv_name = ('64x8-'
                '32x5+2-'
                '!3x5+2')
 
-deconv = build_de_conv_layers((512, 1, 1), 'deconv32', where='output')
+deconv_name = '[x5+2]64x4+0-64-64:2++1-32-32:2++1-32-32:2++1-!3x5+2'
+deconv_name = '[x5+2]64x8+0-64-64:2++1-32-32:2++1-32-!3x5+2'
+
+deconv = build_de_conv_layers((512, 1, 1), deconv_name, where='output')
 deconv.to(device)
 
 x = deconv(torch.randn(1, 512, 1, 1, device=device))
@@ -38,11 +41,21 @@ print('output shape for', deconv.name)
 print(*x.shape[1:], '--', *deconv.output_shape)
 
 
-conv_layers = [str(c) for c in deconv.children() if 'conv' in str(c).lower()]
+conv_layers = [str(c) for c in conv.children() if 'conv' in str(c).lower()]
+
+print('====X====')
+print(*conv.shapes[0])
+for c, s in zip(conv_layers, conv.shapes[1:]):
+
+    print('        ', c)
+    print(*s)
+print('====Z====')
+
+deconv_layers = [str(c) for c in deconv.children() if 'conv' in str(c).lower()]
 
 print('====Z====')
 print(*deconv.shapes[0])
-for c, s in zip(conv_layers, deconv.shapes[1:]):
+for c, s in zip(deconv_layers, deconv.shapes[1:]):
 
     print('        ', c)
     print(*s)

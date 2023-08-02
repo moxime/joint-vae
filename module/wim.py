d9aff1ebea629bd0587bc3ca3e8289c811db13d8
moxime <mossonce@hotmail.com>
2023-08-02 22:50:51 +0200

    tbt

--- module/wim.py
+++ module/wim.py
@@ -1,5 +1,6 @@
 import os
 import torch
+import logging
 from cvae import ClassificationVariationalNetwork as M
 import utils.torch_load as torchdl
 from module.priors import build_prior
@@ -7,13 +8,26 @@ from module.priors import build_prior
 
 class WIMVariationalNetwork(M):
 
-    def __init__(self, *a, alternate_prior={}, **kw):
+    def __init__(self, *a, alternate_prior=None, **kw):
 
         super().__init__(*a, **kw)
         self._original_prior = self.encoder.prior,
-        self._alternate_prior = build_prior(**alternate_prior)
 
-    def alternate_prior_finetune(self, *sets, epochs=5, alpha=0.1, optimizer=None):
+        self._alternate_prior = None
+        if alternate_prior is not None:
+            self.alternate_prior = build_prior(**alternate_prior)
+
+    @property
+    def alternate_prior(self):
+        return self._alternate_prior
+
+    @alternate_prior.setter
+    def alternate_prior(self, p):
+
+        assert self.alternate_prior is None
+        self._alternate_prior = p
+
+    def finetune(self, *sets, epochs=5, alpha=0.1, optimizer=None):
 
         if optimizer is None:
             optimizer = self.optimizer
@@ -93,17 +107,19 @@ class WIMVariationalNetwork(M):
 
 if __name__ == '__main__':
 
+    import sys
     import argparse
     import configparser
     from utils.save_load import find_by_job_number
     from utils.parameters import get_last_jobnumber, register_last_jobnumber
+    from utils.print_log import EpochOutput
 
     conf_parser = argparse.ArgumentParser(add_help=False)
     conf_parser.add_argument('--debug', action='store_true')
     conf_parser.add_argument('--verbose', '-v', action='count', default=0)
     conf_parser.add_argument('--config-file', default='config.ini')
 
-    conf_args, remaining_args = conf_parser.parse_known_args(argv)
+    conf_args, remaining_args = conf_parser.parse_known_args()
 
     config = configparser.ConfigParser()
     config.read(conf_args.config_file)
@@ -121,13 +137,17 @@ if __name__ == '__main__':
     parser.add_argument('--job-dir', default='./jobs')
     parser.add_argument('--job-number', type=int)
 
+    parser.add_argument('--wim-sets', nargs='*')
+
     parser.set_defaults(**defaults)
 
-    args = parser.parse_args()
+    args = parser.parse_args(remaining_args)
 
-    model = find_by_job_number(args.job, job_dir=args.job_dir)
+    model_dict = find_by_job_number(args.job, job_dir=args.job_dir)
 
-    dataset = model['set']
+    dataset = model_dict['set']
+
+    model = WIMVariationalNetwork.load(model['dir'], load_net=True, load_state=True)
 
     job_number = args.job_number
     if not job_number:
@@ -142,14 +162,11 @@ if __name__ == '__main__':
 
     output_file = os.path.join(args.output_dir, f'train-{job_number:06d}.out')
 
-    log.debug(f'Outputs registered in {output_file}')
+    logging.debug(f'Outputs registered in {output_file}')
     outputs = EpochOutput()
     outputs.add_file(output_file)
 
-    while os.path.exists(save_dir):
-        log.debug(f'{save_dir} exists')
-        job_number += 1
-        save_dir = os.path.join(save_dir_root, f'{job_number:06d}')
-
     model.job_number = job_number
     model.saved_dir = save_dir
+
+    model.finetune(*args.wim_sets)

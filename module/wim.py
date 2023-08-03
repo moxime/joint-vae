2001e454225b8ddebc1c1a494f67b9ca4eb49781
moxime <mossonce@hotmail.com>
2023-08-03 16:01:45 +0200

    output

--- module/wim.py
+++ module/wim.py
@@ -5,6 +5,7 @@ from cvae import ClassificationVariationalNetwork as M
 from utils.save_load import MissingKeys
 import utils.torch_load as torchdl
 from module.priors import build_prior
+from utils.print_log import EpochOutput
 
 
 class WIMVariationalNetwork(M):
@@ -44,7 +45,10 @@ class WIMVariationalNetwork(M):
 
         return model
 
-    def finetune(self, *sets, epochs=5, alpha=0.1, optimizer=None):
+    def finetune(self, *sets,
+                 epochs=5, alpha=0.1,
+                 optimizer=None,
+                 outputs=EpochOutput()):
 
         if optimizer is None:
             optimizer = self.optimizer
@@ -55,6 +59,7 @@ class WIMVariationalNetwork(M):
         transformer = self.training_parameters['transformer']
         data_augmentation = self.training_parameters['data_augmentation']
         batch_size = self.training_parameters['batch_size']
+        test_batch_size = self.training_parameters['test_batch_size']
 
         trainset, testset = torchdl.get_dataset(set_name,
                                                 transformer=transformer,
@@ -81,11 +86,23 @@ class WIMVariationalNetwork(M):
 
         device = next(self.parameters()).device
 
+        acc_methods = self.predict_methods
+
+        outputs.results(0, 0, -2, epochs,
+                        metrics=self.metrics,
+                        loss_components=self.loss_components,
+                        acc_methods=acc_methods)
+        outputs.results(0, 0, -1, epochs,
+                        metrics=self.metrics,
+                        loss_components=self.loss_components,
+                        acc_methods=acc_methods)
+
         for epoch in range(epochs):
 
             moving_iters = {_: iter(moving_loaders[_]) for _ in moving_loaders}
             moving_batches = {_: next(moving_iters[_]) for _ in moving_iters}
 
+            per_epoch = len(trainloader)
             for i, (x, y) in enumerate(trainloader):
 
                 optimizer.zero_grad()
@@ -95,6 +112,16 @@ class WIMVariationalNetwork(M):
                                                                    current_measures=current_measures,
                                                                    batch=i,
                                                                    with_beta=True)
+                outputs.results(i, per_epoch, epoch + 1, epochs,
+                                preambule='train',
+                                acc_methods=acc_methods,
+                                loss_components=self.loss_components,
+                                losses=train_mean_loss,
+                                metrics=self.metrics,
+                                measures=measures,
+                                time_per_i=t_per_i,
+                                batch_size=train_batch_size,
+                                end_of_epoch='\n')
 
                 self.encoder.prior = self._alternate_prior
 
@@ -117,10 +144,13 @@ class WIMVariationalNetwork(M):
                 optimizer.clip(self.parameters())
                 optimizer.step()
 
-        sample_dirs = [os.path.join(self.saved_dir, 'samples', d)
-                       for d in ('last', f'{epoch:04d}')]
+        sample_dirs = [os.path.join(self.saved_dir, 'samples', '{:04d}'.format(self.trained)]
 
-        self.ood_detection_rate()
+        self.ood_detection_rate(batch_size=test_batch_size,
+                                num_batch='all',
+                                outputs=outputs,
+                                sample_dirs=sample_dirs,
+                                print_result='*')
 
 
 if __name__ == '__main__':
@@ -130,25 +160,24 @@ if __name__ == '__main__':
     import configparser
     from utils.save_load import find_by_job_number
     from utils.parameters import get_last_jobnumber, register_last_jobnumber, set_log
-    from utils.print_log import EpochOutput
 
-    conf_parser = argparse.ArgumentParser(add_help=False)
+    conf_parser=argparse.ArgumentParser(add_help=False)
     conf_parser.add_argument('--debug', action='store_true')
     conf_parser.add_argument('--verbose', '-v', action='count', default=0)
     conf_parser.add_argument('--config-file', default='config.ini')
 
-    conf_args, remaining_args = conf_parser.parse_known_args()
+    conf_args, remaining_args=conf_parser.parse_known_args()
 
-    config = configparser.ConfigParser()
+    config=configparser.ConfigParser()
     config.read(conf_args.config_file)
 
-    config_params = config['DEFAULT']
+    config_params=config['DEFAULT']
 
-    defaults = {}
+    defaults={}
 
     defaults.update(config_params)
 
-    parser = argparse.ArgumentParser(parents=[conf_parser],
+    parser=argparse.ArgumentParser(parents=[conf_parser],
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     parser.add_argument('job', type=int)
@@ -159,13 +188,13 @@ if __name__ == '__main__':
 
     parser.set_defaults(**defaults)
 
-    args = parser.parse_args(remaining_args)
+    args=parser.parse_args(remaining_args)
 
-    model_dict = find_by_job_number(args.job, job_dir=args.job_dir)
+    model_dict=find_by_job_number(args.job, job_dir=args.job_dir)
 
     if model_dict is None:
         logging.debug('Model not found, reollecting models')
-        model_dict = find_by_job_number(args.job, job_dir=args.job_dir, flash=False)
+        model_dict=find_by_job_number(args.job, job_dir=args.job_dir, flash=False)
 
     if model_dict is None:
         logging.error('Model not found')
@@ -173,40 +202,40 @@ if __name__ == '__main__':
 
     logging.info('Model found')
 
-    dataset = model_dict['set']
+    dataset=model_dict['set']
 
-    model = WIMVariationalNetwork.load(model_dict['dir'], load_net=True, load_state=True)
+    model=WIMVariationalNetwork.load(model_dict['dir'], load_net=True, load_state=True)
 
-    job_number = args.job_number
+    job_number=args.job_number
     if not job_number:
-        job_number = get_last_jobnumber() + 1
+        job_number=get_last_jobnumber() + 1
 
-    log_dir = os.path.join(defaults['output_dir'], 'log')
+    log_dir=os.path.join(defaults['output_dir'], 'log')
 
-    log = set_log(conf_args.verbose, conf_args.debug, log_dir, job_number=job_number)
+    log=set_log(conf_args.verbose, conf_args.debug, log_dir, job_number=job_number)
 
     register_last_jobnumber(job_number)
 
-    save_dir_root = os.path.join(args.job_dir, dataset,
+    save_dir_root=os.path.join(args.job_dir, dataset,
                                  model.print_architecture(sampling=False),
                                  'wim')
 
-    save_dir = os.path.join(save_dir_root, f'{job_number:06d}')
+    save_dir=os.path.join(save_dir_root, f'{job_number:06d}')
 
-    output_file = os.path.join(args.output_dir, f'train-{job_number:06d}.out')
+    output_file=os.path.join(args.output_dir, f'train-{job_number:06d}.out')
 
     logging.debug(f'Outputs registered in {output_file}')
-    outputs = EpochOutput()
+    outputs=EpochOutput()
     outputs.add_file(output_file)
 
-    model.job_number = job_number
-    model.saved_dir = save_dir
+    model.job_number=job_number
+    model.saved_dir=save_dir
 
     model.encoder.prior.mean.requires_grad_(False)
-    alternate_prior_params = model.encoder.prior.params
-    alternate_prior_params['learned_means'] = False
-    alternate_prior_params['init_mean'] = 0.
+    alternate_prior_params=model.encoder.prior.params
+    alternate_prior_params['learned_means']=False
+    alternate_prior_params['init_mean']=0.
 
-    model.alternate_prior = alternate_prior_params
+    model.alternate_prior=alternate_prior_params
 
     model.finetune(*args.wim_sets)

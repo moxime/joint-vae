769d7097f11ee3676f9257d144adce5f7041c0e8
moxime <mossonce@hotmail.com>
2023-08-23 15:38:19 +0200

    test alternate

---
+++ tests/test_alternate_prior.py
@@ -0,0 +1,48 @@
+import logging
+from module.wim import WIMVariationalNetwork as W
+from utils.save_load import find_by_job_number
+from utils.torch_load import get_batch, get_dataset
+import argparse
+
+if __name__ == '__main__':
+
+    logging.getLogger().setLevel(20)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('job', type=int)
+    parser.add_argument('batch', type=int, default=100)
+    args = parser.parse_args()
+
+    d = find_by_job_number(args.job, load_net=False)['dir']
+
+    m = W.load(d, load_state=True, load_net=True)
+
+    alternate_prior = m.encoder.prior.params
+
+    m.set_alternate_prior(alternate_prior)
+
+    logging.info('From {} to {}'.format(m.original_prior(), m.alternate_prior()))
+    logging.info('{:.4} -> {:.4}'.format(m.original_prior().mean.var(0).mean(),
+                                         m.alternate_prior().mean.var(0).mean()))
+
+    _, dset = get_dataset(m.training_parameters['set'])
+
+    x, y = get_batch(dset, batch_size=args.batch)
+
+    m.eval()
+
+    m.original_prior()
+    _, _, losses, _ = m.evaluate(x)
+
+    print(*losses['total'].shape)
+
+    print('original')
+
+    print('total: {:.4}, kl: {:.4}'.format(*(losses[_].min(0)[0].mean() for _ in ('total', 'kl'))))
+
+    m.alternate_prior()
+    _, _, losses, _ = m.evaluate(x)
+
+    print('alternate')
+
+    print('total: {:.4}, kl: {:.4}'.format(*(losses[_].min(0)[0].mean() for _ in ('total', 'kl'))))

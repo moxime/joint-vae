bda1170da076198a1f1b8fd74105df5959114000
moxime <mossonce@hotmail.com>
2023-09-13 17:05:55 +0200

    clean output to be tested

--- utils/print_log.py
+++ utils/print_log.py
@@ -53,7 +53,7 @@ class EpochOutput:
     END_OF_EPOCH = 10
     END_OF_SET = 0
 
-    CELL_WIDTH = 11
+    CELL_WIDTH = 9
 
     unix_tags = [('%B', '\033[1m'),
                  ('%b', '\033[0m'),
@@ -65,11 +65,20 @@ class EpochOutput:
                      ('%I', ''),
                      ('%i', '')]
 
+    cell_formats = {'metrics': '{{{sep}{c}.2e}}'.format(sep=':', c=CELL_WIDTH),
+                    'losses': '{{{sep}{c}.2e}}'.format(sep=':', c=CELL_WIDTH),
+                    'accuracy': '{{{sep}{c}.2%}}'.format(sep=':', c=CELL_WIDTH),
+                    'dB': '{{{sep}{c}.1f}} dB'.format(sep=':', c=CELL_WIDTH - 3),
+                    'odin': '{{{sep}>{c}}}'.format(sep=':', c=CELL_WIDTH),
+                    'text': '{{{sep}>{c}}}'.format(sep=':', c=CELL_WIDTH),
+                    }
+
     def __init__(self):
 
         stdout = {'stream': sys.stdout, 'when': self.EVERY_BATCH, 'tags': self.unix_tags}
         self.streams = [stdout]
         self.files = []
+        self.last_row = []
 
     def format_encoding(self, string, tags=no_style_tags):
 
@@ -95,180 +104,92 @@ class EpochOutput:
                     f_.write(self.format_encoding(string, f['tags']))
                     f_.flush()
 
-    def results(self, i, per_epoch, epoch, epochs,
-                loss_components=None,
-                masked_loss_components=['z_logdet', 'z_mahala', 'z_tr_inv_cov'],
-                losses={},
-                acc_methods=(),
-                accuracies={},
-                metrics=(),
-                measures={},
-                best_of={'odin': -1},
-                time_per_i=0, batch_size=100,
-                preambule='',
-                end_of_epoch='\n'):
+    def result_col(self, title, default_format='text', header=False, masked=[],
+                   sep=' | ',
+                   **kv):
 
-        if loss_components:
-            loss_components = [_ for _ in loss_components if _ not in masked_loss_components]
+        default_cell_format = self.cell_formats.get(default_format) or self.cell_formats['text']
+        col_width = self.CELL_WIDTH
+
+        if header:
+            h1 = sep.join('{k:_^{w}}'.format(k=k, w=col_width) for k in kv if k not in masked)
+            h0 = '{t:_^{w}}'.format(t=title, w=len(h1))
+            return h0 if header == 2 else h1
+        else:
+            return sep.join(self.cell_formats.get(k, default_cell_format).format(kv[k])
+                            for k in kv if k not in masked)
+
+    def result_row(self, header=False, masked=[], sep=' | ', double_sep=' || ', **kvs):
+
+        self.last_row = [*kvs]
+
+        return double_sep.join(self.result_col(k, header=header,
+                                               masked=masked,
+                                               default_format=k,
+                                               sep=sep,
+                                               **kvs[k])
+                               for k in kvs)
+
+    def results(self, batch, batches, epoch, epochs,
+                loss_components=None,
+                masked_components=['z_logdet', 'z_mahala', 'z_tr_inv_cov'],
+                best_of={'odin': -1},
+                time_per_i=0,
+                batch_size=100,
+                preambule='',
+                end_of_epoch='\n',
+                **kvs,
+                ):
+
         if preambule == 'train':
             preambule = '%I' + preambule
             end_of_format = '%i'
-            sep = '%i' + '|' + '%I'
-            double_sep = '%i' + '||' + '%I'
+            sep = '%i' + ' | ' + '%I'
+            double_sep = '%i' + ' || ' + '%I'
         else:
             end_of_format = ''
-            sep = '|'
-            double_sep = '||'
+            sep = ' | '
+            double_sep = ' || '
 
-        K_epochs = 5
-        cell_width = self.CELL_WIDTH
-        K_preambule = 9
-        num_format = {'default': '{' + f':{cell_width-1}.2e' + '} ',
-                      'odin': '{' + f':>{cell_width}' + '}',
-                      'dB': '{' + f':{cell_width-4}.1f' + '} dB '}
+        kepts_kvs = {'': {'epoch': '{}/{}'.format(epoch, epochs) if preambule.lower() == 'train' else '',
+                          '': preambule}}
 
-        kept_metrics = {}
-        for k in metrics:
-            if '-a-' not in k and not k.endswith('-2s'):
-                kept = True
-                for k_ in best_of:
-                    if k.startswith(k_):
-                        kept_metrics[k_] = None
-                        kept = False
-                if kept:
-                    kept_metrics[k] = None
-        metrics = list(kept_metrics)
-
-        kept_accuracies = {}
-        kept_methods = {}
-        which_is_best = {}
-
-        for k in acc_methods:
-            kept = True
-            for k_ in best_of:
-                if k.startswith(k_):
-                    kept_methods[k_] = None
-                    _s = best_of[k_]
-                    # ex _s = -1 ; acc = 29 k_acc = inf
-                    # ex _s = -1 ; acc = inf k_acc = 29
-                    # ex _s = 1 ; acc = 29 k_acc = -inf
-                    if _s * accuracies.get(k, -_s * np.inf) > _s * kept_accuracies.get(k_, -_s * np.inf):
-                        # print('***     best {:16} {:4.1f}'.format(k, 100 * accuracies.get(k, -_s * np.inf)))
-                        kept_accuracies[k_] = accuracies[k]
-                        measures[k_] = k[len(k_) + 1:]
-                    else:
-                        pass
-                        # print('*** not best {:16} {:4.1f}'.format(k, 100 * accuracies.get(k, -_s * np.inf)))
-                    kept = False
-
-            if kept:
-                kept_methods[k] = None
-                if k in accuracies:
-                    kept_accuracies[k] = accuracies[k]
-
-        acc_methods = list(kept_methods)
-        accuracies = kept_accuracies
-
-        # print('*** acc_m', *acc_methods)
-        if epoch == -2:
-            i = per_epoch - 1
-            preambule = f'{"epoch":_^{2 * K_epochs}}{preambule:_>{K_preambule}}_'
-            if loss_components:
-                length = len(sep.join(f'{k:^{cell_width}}' for k in loss_components))
-                loss_str = f'{"losses":_^{length}}'
-            else:
-                loss_str = ''
-            if metrics:
-                length = len(sep.join(f'{k:^{cell_width}}' for k in metrics))
-                metrics_str = f'{"metrics":_^{length}}'
-            else:
-                metrics_str = ''
-
-        elif epoch == -1:
-            i = per_epoch - 1
-            preambule = f'{" ":^{2 * K_epochs}}{preambule:>{K_preambule}} '
-
-            if loss_components:
-                length = len(sep.join(f'{k:^{cell_width}}' for k in loss_components))
-                loss_str = sep.join(f'{k:^{cell_width}}' for k in loss_components)
-            else:
-                loss_str = ''
-            if metrics:
-                length = len(sep.join(f'{k:^{cell_width}}' for k in metrics))
-                metrics_str = sep.join(f'{k:^{cell_width}}' for k in metrics)
-            else:
-                metrics_str = ''
-        else:
-            if epoch:
-                preambule = f'{epoch:{K_epochs}d}/{epochs:<{K_epochs}d} {preambule:>{K_preambule}} '
-            else:
-                preambule = f'{preambule:>{K_preambule + 2 * K_epochs}} '
-
-            if loss_components:
-
-                if losses:
-                    formatted = {k: num_format.get(k, num_format['default'])
-                                 for k in loss_components}
-                    value = {k: losses.get(k, np.nan)
-                             for k in loss_components}
-
-                    loss_str = sep.join(formatted[k].format(value[k])
-                                        for k in loss_components)
-                else:
-                    loss_str = sep.join(cell_width * ' ' for k in loss_components)
-            else:
-                loss_str = ''
-
-            if metrics:
-                if measures:
-                    formatted = {k: num_format.get(k, num_format['default'])
-                                 for k in metrics}
-                    value = {k: measures.get(k, np.nan)
-                             for k in metrics}
-
-                    metrics_str = sep.join(formatted[k].format(value[k])
-                                           for k in metrics)
-                else:
-                    metrics_str = sep.join(cell_width * ' ' for k in metrics)
-
-            else:
-                metrics_str = ''
-
-        if epoch == -2:
-            length = len(sep.join(f'{k:^9}' for k in acc_methods))
-            acc_str = f'{"accuracy":_^{length}}'
-        elif epoch == -1:
-            acc_str = sep.join(f'{k:^9}' for k in acc_methods)
-        elif accuracies:
-            acc_str_ = []
-            for k in acc_methods:
-                acc_str_.append(f' {accuracies[k]:7.2%} ')
-            acc_str = sep.join(acc_str_)
-        else:
-            acc_str = sep.join(9 * ' ' for k in acc_methods)
+        for title in kvs:
+            done_best = []
+            kept_kvs[title] = {}
+            for k in kvs[title]:
+                if k in masked_components:
+                    continue
+                if title == 'metrics' and '-a-' in k or k.endswith('-2s'):
+                    continue
+                for best_k in best_of:
+                    if k.startswith(best_k) and best_k not in done_best:
+                        kept_kvs[title][best_k] = _s * max(_s * kvs[title][_]
+                                                           for _ in kvs[title] if _.startswith(best_k))
+                        done_best.append(best_k)
+                    continue
+                kept_kvs[title][k] = kvs[title][k]
 
         if time_per_i > 0:
             time_per_i = Time(time_per_i)
-            if i < per_epoch - 1:
-                eta_str = f' {time_per_i / batch_size:>9}/i'
-                eta_str += f'   eta: {time_per_i * (per_epoch - i):<9}'
+            kept_kvs['time']['/i'] = time_per_i
+
+            if batch < batches - 1:
+                kept_kvs['time']['eta'] = time_per_i * batch_size * (batches - batch)
             else:
-                eta_str = f' {time_per_i / batch_size:>9}/i'
-                eta_str += f' total: {time_per_i * per_epoch:<9}'
+                kept_kvs['time']['total'] = time_per_i * batch_size * batches
 
-        else:
-            eta_str = ' '
+        if not batch:
+            if self.last_row != [*kept_kvs]:
+                line = '\r' + self.result_row(header=2, sep=sep, double_sep=double_sep, **kept_kvs)
+                self.write(line + '\n', when=self.END_OF_EPOCH)
+            line = '\r' + self.result_row(header=1, sep=sep, double_sep=double_sep, **kept_kvs)
+            self.write(line + '\n', when=self.END_OF_EPOCH)
 
-        strings = [s for s in [preambule,
-                               loss_str,
-                               metrics_str,
-                               acc_str + end_of_format,
-                               eta_str + end_of_format] if s]
-
-        line = '\r' + double_sep.join(strings) + ' '
+        line = '\r' + self.result_row(header=False, sep=sep, double_sep=double_sep, **kept_kvs)
         self.write(line, when=self.EVERY_BATCH)
 
-        if i == per_epoch - 1:
+        if batch == batches - 1:
             self.write(line + '\n', when=self.END_OF_EPOCH)
 
 
@@ -296,7 +217,7 @@ class Time(float):
 
         t = self
 
-        units = ['d', 'h', 'm', 's', 'ms', 'mus', 'ns']
+        units = ['d', 'h', 'm', 's', 'ms', '\u03bcs', 'ns']
         qs = [24 * 3600, 3600, 60, 1, 1e-3, 1e-6, 1e-9]
 
         if t == 0:
@@ -305,22 +226,15 @@ class Time(float):
         str = '-' if t < 0 else ''
         t = abs(t)
 
-        break_loop = False
-
-        coarser = None
+        orig_t = t
         for i, (unit, q) in enumerate(zip(units, qs)):
 
-            if coarser is not None and i - coarser >= max - 1:
-                n = int(np.round(t / q))
-                break_loop = True
-            else:
-                n = int(t / q)
+            n = int(t / q)
             if n:
                 str += f'{n}{unit}'
-                if coarser is None:
-                    coarser = i
             t -= q * n
-            if break_loop:
+
+            if t <= orig_t / 100:
                 break
 
         return str
@@ -361,7 +275,7 @@ def text_dataviz(start, end, *marks, N=100, min_val=None, max_val=None, default=
 
     def f2i(x):
         i = int(N * (x - min_val) / width)
-        return max(min(i, N-1), 0)
+        return max(min(i, N - 1), 0)
 
     start_ = f2i(start)
     end_ = f2i(end)
@@ -377,7 +291,7 @@ def text_dataviz(start, end, *marks, N=100, min_val=None, max_val=None, default=
 def timerun(func):
     """ Calculate the execution time of a method and return it back"""
 
-    @functools.wraps(func)
+    @ functools.wraps(func)
     def wrapper(*args, **kwargs):
         start = time.time()
         result = func(*args, **kwargs)
@@ -389,4 +303,13 @@ def timerun(func):
 
 
 if __name__ == '__main__':
-    pass
+
+    kvs = {'': {'epoch': '400/600', '': 'VALID'},
+           'losses': {'total': -1.2332e-1, 'kl': 32.5454e-2, 'dB': -52.22325, 'odin': 'yes!'},
+           'accuracy': {'iws': 0.84846, 'iws-2': 0.987, 'foo': 0.6545},
+           'time': {'/i': Time(122.1511e-6), 'eta': Time(617.65154)}
+           }
+
+    o = EpochOutput()
+    for header in (2, 1, False, False, False, False):
+        print(o.result_row(header=header, masked=['foo'], **kvs))

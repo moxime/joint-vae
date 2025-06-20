e70afcc975359bda5fcd9adef0693b660afb28f0
moxime <mossonce@hotmail.com>
2025-06-20 16:32:54 +0200

    lot of work to merge

--- ft/results/scod.py
+++ ft/results/scod.py
@@ -16,6 +16,34 @@ default_scores['wim'] = {'r': 'zdist', 'g': 'elbo'}
 default_scores['vib'] = {'r': 'odin-1-0.0040', 'g': 'none'}
 
 
+PRINTSTAT = True
+PRINTSTAT = False
+
+
+def printstat(func):
+
+    def wrapped(*a, **kw):
+
+        scores = func(*a, **kw)
+
+        score = kw.get('score', '--')
+        T = kw.get('T', 1.)
+
+        try:
+            smin = scores.min()
+            smax = scores.max()
+            smean = scores.mean()
+            sstd = scores.std()
+            if PRINTSTAT:  # 'msp' in score and 'log' not in score:
+                print('*** {}-{} [{:.2g}--{:.2g}] {:.2g}+/-{:.2g}'.format(score, T, smin, smax, smean, sstd))
+        except AttributeError:
+            pass
+
+        return scores
+
+    return wrapped
+
+
 def grid_search_odin(in_rec, *out_rec, metrics='scod', tpr=0.95):
 
     assert metrics in ('fpr', 'sel', 'scod')
@@ -89,66 +117,89 @@ def scrisk(y_true, y_est, r_scores, g_scores, weight=0.5, n_tpr=1000):
     scores = weight * g_ + (1 - weight) * r_scores.unsqueeze(-1)
 
     in_samples = (y_true >= 0).sum()
+    acc = (y_est == y_true).float().sum()/in_samples
+
     out_samples = len(y_true) - in_samples
 
     thresholds = torch.zeros(n_tpr)
+
+    scores_ = {'g': g_scores, 'r': r_scores}
+    i_ = {'in': y_true >= 0, 'out': y_true < 0}
+
+    for i in i_:
+        for s in scores_:
+            _score = scores_[s][i_[i]]
+            _q = _score.quantile(torch.tensor([0.05, 0.25, 0.5, 0.75, 0.95]))
+            __q = '--'.join(map('{: .2e}'.format, _q))
+            print('{}[{:3}]: [{}] {: .2e} +/-{:.1e}'.format(s, i, __q,  _score.mean(),
+                                                            _score.std()))
 
     for i in range(n_tpr):
 
         i_thr = max(-int(-(checked_tpr[i] * in_samples - 1)), 0)
         thresholds[i] = scores[y_true >= 0, i].sort()[0][i_thr]
+        # print('{:.1%} -- {:7.2e}'.format(checked_tpr[i], thresholds[i]))
 
-    # i_pos n_samples x n_tpr
-    # pos : s(x) <= threshold
+    # i_pos: n_samples x n_tpr
+    # pos: s(x) <= threshold
     i_pos = scores <= thresholds.unsqueeze(0)
 
     i_true_pos = (i_pos & (y_true >= 0).unsqueeze(-1))
 
     i_false_pos = (i_pos & (y_true < 0).unsqueeze(-1))
 
-    classif_errors = i_pos & ((y_true >= 0) & (y_true != y_est)).unsqueeze(-1)
+    classif_errors_as_pos = i_pos & ((y_true >= 0) & (y_true != y_est)).unsqueeze(-1)
 
     tpr = i_true_pos.sum(0) / in_samples
     fpr = i_false_pos.sum(0) / out_samples
-    selective_risk = classif_errors.sum(0) / tpr / in_samples
+    selective_risk = classif_errors_as_pos.sum(0) / tpr / in_samples
 
-    return tpr, selective_risk, fpr
+    return tpr, selective_risk, fpr, acc
 
 
-def scoring_r(losses, score='msp', y_est=None, mtype='cvae'):
+@printstat
+def scoring_r(losses, score='msp', y_est=None, mtype='cvae', T=1.):
     """ estimation of 1 - max P(y|x)
     """
+
+    # print('*** r', score, mtype)
+
     if score == 'default':
         score = default_scores[mtype]['r']  #
 
     logging.debug('r score is {}'.format(score))
 
-    # for _ in losses:
-    #     if _.startswith('odin'):
-    #         print('{} {:.2f}--{:.2f}'.format(_, losses[_].min(), losses[_].max()), *losses[_].shape)
-
     if score and score.startswith('odin'):
 
-        # print('***', losses[score].shape, losses[score].mean())
         return 1 - losses[score]
 
     if score is None:
-        return 0.
+        return None
 
     if y_est is None:
         y_est = losses['y_est_already']
 
     if score == 'predist':
-        return 0.5 * losses['pre-zdist'].gather(0, y_est.unsqueeze(0)).squeeze()
+        return 0.5 * losses['pre-zdist'].gather(0, y_est.unsqueeze(0)).squeeze() / T
 
-    if score == 'zdist':
-        return 0.5 * losses['zdist'].gather(0, y_est.unsqueeze(0)).squeeze()
+    if score == 'dist':
+        return 0.5 * losses['zdist'].gather(0, y_est.unsqueeze(0)).squeeze() / T
 
     if score == 'logmsp':
-        return scoring_r(losses, 'dist', y_est=y_est) - (-0.5 * losses['zdist']).logsumexp(0)
+        return scoring_r(losses, score='dist', y_est=y_est, T=T) + (-0.5 * losses['zdist'] / T).logsumexp(0)
+
+    if score == 'prelogmsp':
+        return (scoring_r(losses, score='predist', y_est=y_est, T=T)
+                + (-0.5 * losses['pre-zdist'] / T).logsumexp(0))
 
     if score == 'msp':
         return 1 - losses['logits'].softmax(dim=0).max(dim=0)[0]
+
+    if 'msp-' in score:
+        T = float(score.split('-')[1])
+        score = score.split('-')[0].replace('msp', 'logmsp')
+        # print('***', score, T)
+        return 1 - (-scoring_r(losses, score=score, y_est=y_est, mtype=mtype, T=T)).exp()
 
     raise ValueError('{} is unknwon for r(x)'.format(score))
 
@@ -156,6 +207,7 @@ def scoring_r(losses, score='msp', y_est=None, mtype='cvae'):
 def scoring_g(losses, score='elbo', y_est=None, mtype='wim'):
     """ estimation of pOut/pIn
     """
+    # print('*** g', score, mtype)
 
     if score == 'default':
         score = default_scores[mtype]['g']
@@ -163,7 +215,7 @@ def scoring_g(losses, score='elbo', y_est=None, mtype='wim'):
     logging.debug('g score is {}'.format(score))
 
     if score is None:
-        return 0.
+        return None
 
     def scoring_alt(k):
 
@@ -184,7 +236,7 @@ def scoring_g(losses, score='elbo', y_est=None, mtype='wim'):
     """ sign has to be >0 if the score is higher for ood
     """
     sign = 1
-    if score == 'elbo':
+    if score.startswith('elbo'):
         key = 'total'
         sign = 1
 
@@ -194,7 +246,7 @@ def scoring_g(losses, score='elbo', y_est=None, mtype='wim'):
     if score == 'g':
         sign = 1
 
-    if 'wim' in mtype and key.endswith('~@'):
+    if 'wim' in mtype and score.endswith('~@'):
         # score has to be high for ood
         return sign * (scoring_in(key) - scoring_alt(key))
 
@@ -255,7 +307,7 @@ if __name__ == '__main__':
     parser.add_argument('-w', '--weight', default=0.5, type=float)
     parser.add_argument('-v', action='count', default=0)
     parser.add_argument('-f', action='store_true')
-    parser.add_argument('--tab', nargs='?', const='/dev/stdout')
+    parser.add_argument('--tab', nargs='?', const='/dev/stdout', default='/dev/null')
 
     args = parser.parse_args()
 
@@ -263,6 +315,11 @@ if __name__ == '__main__':
 
     """ ^^ CONFIG ^^
     """
+
+    for j in jobs:
+
+        if jobs[j]['mdict'] is None:
+            logging.error('{} not found'.format(j))
 
     for j in jobs:
         print(j, jobs[j]['r'], jobs[j]['g'])
@@ -306,9 +363,40 @@ if __name__ == '__main__':
 
     for j in jobs:
 
-        print('\n{:=^80}'.format(j))
+        mdict = jobs[j]['mdict']
+        dset = mdict['set']
+
+        model = mdict['net']
+
+        mtype = ''
+        if type(model).__name__.lower().endswith('array'):
+            mtype = type(model).__name__.lower()[:-5] + '-'
+
+        rdir = model_subdir(mdict, 'samples', '{:04}'.format(mdict['done']))
+
+        files = os.listdir(rdir)
+
+        logging.debug('{}: {}'.format(rdir[-20:], ' - '.join(files)))
+        rec = LossRecorder.loadall(rdir, device='cpu')
+
+        allsets_ = list(rec)
+
+        allsets = [dset] + oodsets
+
+        mtype += mdict['type']
+
+        if not set(allsets) <= set(allsets_):
+            logging.error('Sets missing: {}'.format(' '.join(set(allsets) - set(allsets_))))
+            continue
+
+        print('\n{:=^70}{:>30}'.format(j, ' '.join(allsets)))
 
         for r, g in product(jobs[j]['r'], jobs[j]['g']):
+
+            if r and r.lower() == 'none':
+                r = None
+            if g and g.lower() == 'none':
+                g = None
 
             if not (r or g):
                 continue
@@ -316,45 +404,19 @@ if __name__ == '__main__':
             tab_row = '{}-{}-{}'.format(j, r, g)
             tex_tab.append_cell(texify(tab_row), row=tab_row)
 
-            print('\n{:=^80}'.format('r:{} g:{}'.format(r, g)))
+            print('\n{:_^100}'.format('r:{} g:{}'.format(r, g)))
 
             logging.info('Scores r:{} g:{}'.format(r, g))
-
-            mdict = jobs[j]['mdict']
-            dset = mdict['set']
-
-            model = mdict['net']
-
-            mtype = ''
-            if type(model).__name__.lower().endswith('array'):
-                mtype = type(model).__name__.lower()[:-5] + '-'
-
-            mtype += mdict['type']
-
-            rdir = model_subdir(mdict, 'samples', '{:04}'.format(mdict['done']))
-
-            files = os.listdir(rdir)
-
-            logging.debug('{}: {}'.format(rdir[-20:], ' - '.join(files)))
-            rec = LossRecorder.loadall(rdir)
-
-            allsets_ = list(rec)
-
-            allsets = [dset] + oodsets
-
-            if not set(allsets) <= set(allsets_):
-                logging.error('Sets missing: {}'.format(' '.join(set(allsets) - set(allsets_))))
-                continue
 
             logging.info('Sets: {}'.format('/'.join(allsets)))
 
             for s in allsets_:
-                if 'y_est_already' not in rec[s]:
-                    rec[s]._tensors['y_est_already'] = rec[s]['cross_y'].argmin(0)
-
-            # for s in allsets:
-            #     print('{:=^20}'.format(s))
-            #     print('\n'.join([_ for _ in rec[s] if _.startswith('pre-')]))
+                if 'cvae' in mtype:
+                    if 'pre-zdist' in rec[s]:
+                        rec[s]._tensors['y_est_already'] = rec[s]['pre-zdist'].argmin(0)
+                    else:
+                        rec[s]._tensors['y_est_already'] = rec[s]['pre-zdist'].argmin(0)
+                assert 'y_est_already' in rec[s]
 
             y_est = torch.hstack([rec[_]['y_est_already'] for _ in allsets])
 
@@ -363,17 +425,21 @@ if __name__ == '__main__':
                 r = grid_search_odin(rec[dset], *[rec[_] for _ in oodsets], metrics='sel')
 
             logging.warning('r={}'.format(r))
-            r_scores = torch.hstack([scoring_r(rec[_], score=r, mtype=mtype)
-                                     for _ in allsets])
+
+            r_scores = None
+            g_scores = None
+
+            if r:
+                r_scores = torch.hstack([scoring_r(rec[_], score=r, mtype=mtype)
+                                         for _ in allsets])
             if g:
                 g_scores = torch.hstack([scoring_g(rec[_], score=g, mtype=mtype)
                                          for _ in allsets])
-            else:
-                g_scores = None
+
             y_true = torch.hstack([rec[_]['y_true'] * int(_ == dset) - int(_ != dset)
                                    for _ in allsets])
 
-            tpr, sr, fpr = scrisk(y_true, y_est, r_scores, g_scores)
+            tpr, sr, fpr, acc = scrisk(y_true, y_est, r_scores, g_scores)
 
             fpr95 = fpr[tpr >= 0.95].min()
             sr95 = sr[tpr >= 0.95].min()
@@ -381,7 +447,8 @@ if __name__ == '__main__':
             auscodrt = auc(tpr, 0.5 * sr + 0.5 * fpr)
             ausrt = auc(tpr, sr)
 
-            _s = f'FPR@95 = {fpr95:.1%} -- SR95 = {sr95:.1%} -- '
+            _s = f'Acc = {acc:.1%} -- '
+            _s += f'FPR@95 = {fpr95:.1%} -- SR95 = {sr95:.1%} -- '
             _s += f'AuROC = {auroc: .1%} '
             _s += f'-- AuST = {ausrt: .1%}'
             _s += f'-- AuSCODT = {auscodrt: .1%}'
